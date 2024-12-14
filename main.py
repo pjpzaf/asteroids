ceeda38fc9e96a46f95700468dbd4cf47ef61383
pjpzaf <paolojosemari.zafra@gmail.com>
2024-12-14 19:28:46 +0800

    added .py files that support main game functions such as moving the sprite, rotating, etc.

--- main.py
+++ main.py
@@ -3,24 +3,40 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    new_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-        surface.fill((0,0,255))
-        screen.blit(surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
-        pygame.display.flip()
+        screen.fill((0,0,0))
         
+        for draw in drawable:
+            new_player.draw(screen) 
+
+        for update in updatable:
+            new_player.update(dt)
+
+        
+        dt = game_clock.tick(60)/1000
+        pygame.display.flip()
 
 
 
